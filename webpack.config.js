0a4ed6965dbb5fd14f4d4e690c4f40737b49b1eb
khaimov <haimovroman12@yandex.ru>
2019-09-08 19:50:32 +0300

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const SOURCE_PATH = path.resolve(__dirname, 'src');
+const DIST_PATH = path.resolve(__dirname, 'dist');
+
+module.exports = {
+    entry: SOURCE_PATH,
+    output: {
+        path: DIST_PATH,
+        filename: 'index.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader',
+                options: {
+                    outDir: DIST_PATH,
+                    declaration: true
+                }
+            }
+        ]
+    }
+};
